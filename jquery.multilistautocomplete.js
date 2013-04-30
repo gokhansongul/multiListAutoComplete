ea3abc46239b9c87cc3f9f1ba441d9ed07d56855
Gokhan Turunc <gokhan.turunc@prime.com.tr>
2013-04-30 16:52:43 +0300

    "config" was created.

--- jquery.multilistautocomplete.js
+++ jquery.multilistautocomplete.js
@@ -1,10 +1,27 @@
 (function($) {
 
     /**
-     * @param {string} selector
      * @param {Object} options
      */
-    $.fn.multiListAutoComplete = function(selector, options){
-        
+    $.fn.multiListAutoComplete = function(options){
+        var $element = $(this);
+
+        /**
+         * @type {Object}
+         */
+        var config = {
+            proxy: this.proxy,
+            listCount: 1,
+            delayTime: 500,
+            minLength: 3,
+            itemPerList: 10,
+            onSelect: this.onSelect,
+            eventType: 'keydown'
+        };
+
+        $element.bind(eventType, function() {
+
+        });
+
     };
 })(jQuery);
